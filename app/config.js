406f3d7f4db68d5259714ff4ff1b6b3ccb2b32f2
Muhammad Fadly <davit9747@gmail.com>
2024-04-26 13:20:57 +0700

    send kode otp dengan nodemailer

--- app/config.js
+++ app/config.js
@@ -5,4 +5,6 @@ module.exports = {
   urlDb: process.env.URL_MONGODB_DEV,
   jwtExpiration: "2h",
   jwtSecret: process.env.jwtSecret,
+  gmail: process.env.GMAIL,
+  password: process.env.PASSWORD,
 };
